43447d45d29e433e57b680281ecd02820f431ce0
FlAerni <florian.aerni@students.fhnw.ch>
2019-11-16 16:11:59 +0100

    Inital Commit

---
+++ src/test/java/rocks/process/acrm/DigiprAcrmApiApplicationTests.java
@@ -0,0 +1,12 @@
+package rocks.process.acrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiprAcrmApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
